402e4128d0351c6e5fafd02505ba498070243ffb
vf12776-ux <vf12776-ux@users.noreply.github.com>
2026-05-03 17:12:26 +0300

    PWA с редиректом на rusalochkasushi.ru

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rusalochka-v1';
+const CACHE_NAME = 'rusalochka-v3';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -9,14 +9,12 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
