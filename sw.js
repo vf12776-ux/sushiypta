4d69829e18d21657e9638265d1819ba21cd0903a
vf12776-ux <vf12776-ux@users.noreply.github.com>
2026-05-04 16:07:42 +0300

    Добавлены новые иконки

--- sw.js
+++ sw.js
@@ -3,10 +3,9 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/icon36-192.png',
+  '/icon36-512.png'
 ];
-
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
